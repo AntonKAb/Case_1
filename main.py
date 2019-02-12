310e12c637ad5a579202d9861f02300442f97cfc
Anton Kabaev <43210849+AntonKAb@users.noreply.github.com>
2019-02-12 13:04:59 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,14 @@ Developers: Anufrienko K., Kabaev A., Lankevich S.
 
 # TODO
 # Government object.
+class state:
+    money = 1000
+    seed = 10000
+    people = 200
+    distemper = 0
+    year = 0
+    land = 130
+
 
 # TODO
 # Events functions.
